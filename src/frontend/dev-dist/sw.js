c7ed42ded76c53195661c6001cc5409f01ad86cd
ReyJrFerrer <2232722@slu.edu.ph>
2025-10-17 10:49:36 +0800

    hi

--- src/frontend/dev-dist/sw.js
+++ src/frontend/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-ff1c5cce'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.48e15dp8kr8"
+    "revision": "0.gt0bjvvnt3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
