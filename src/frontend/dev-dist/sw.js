02da4a4a3bb77c5ddf0238fd51de7f8c4a7944dd
Ron Arvene Flordeliz <2235122@slu.edu.ph>
2025-10-20 10:05:42 +0800

    sw dev-dist

--- src/frontend/dev-dist/sw.js
+++ src/frontend/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-ff1c5cce'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9oul295h40g"
+    "revision": "0.5hqhu61uc4k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
