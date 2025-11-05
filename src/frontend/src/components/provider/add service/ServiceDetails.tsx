ec8d64f3b06599eb0965cabc406a4eeb5b15e790
Ron Arvene Flordeliz <2235122@slu.edu.ph>
2025-11-05 15:37:13 +0800

    readability format

--- src/frontend/src/components/provider/add service/ServiceDetails.tsx
+++ src/frontend/src/components/provider/add service/ServiceDetails.tsx
@@ -515,7 +515,7 @@ const ServiceDetails: React.FC<ServiceDetailsProps> = ({
                               )
                             }
                             required
-                            className={`mt-1 block w-full rounded-md border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-400 text-sm ${
+                            className={`mt-1 block w-full rounded-md border px-3 py-2 text-sm shadow-sm focus:ring-2 focus:ring-blue-400 ${
                               pkgError &&
                               pkgError.name &&
                               !hidePackageFieldError[pkg.id]?.name
